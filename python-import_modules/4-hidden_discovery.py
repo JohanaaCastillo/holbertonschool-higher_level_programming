b74a84145d4d3a0486e7d19bd6d72ede7454372d
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-14 15:37:29 -0500

    update

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4 as hidden
-    list = dir(hidden)
+    import hidden_4
+
+    
     for x in range(len(list)):
         if(list[x][0] != '_'):
             print(list[x])
