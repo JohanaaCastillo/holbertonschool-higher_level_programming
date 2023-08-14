8a7e86352f60c65c20441bd4eb640b0a2ea4eca5
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-14 15:34:44 -0500

    update the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = sum(int(sys.argv[x]) for x in range(1, len(sys.argv)))
+    import sys   
+    result = 0         
+    for x in range(len(sys.argv) - 1):
+        result = sum(int(sys.argv[x + 1]))
     print("{}".format(result))
