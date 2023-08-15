1481f04f3d696b599a99f99effac08dd7faa1433
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-15 11:19:02 -0500

    Update the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     result = 0         
 for x in range(len(sys.argv) - 1):
-    result = sum(int(sys.argv[x + 1]))
-    print("{}".format(result))
+    result = result + int(sys.argv[x + 1])    
+print("{}".format(result))
