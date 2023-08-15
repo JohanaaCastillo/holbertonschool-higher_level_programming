6934d7f39134ec9eb437680429a7189ca8009b80
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-14 22:31:45 -0500

    update pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    
     for x in range(len(list)):
-        if(list[x][0] != '_'):
+        if (list[x][0] != '_'):
             print(list[x])
