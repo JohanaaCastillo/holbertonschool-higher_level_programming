7f2ea3f1e199ab817784cbe933d5b9c732553d9c
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-15 17:02:58 -0500

    Update

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This is a module for square"""
+
+
+class Square:
+    """
+    Creates a square class object
+    """
+    def __init__(self, size):
+        self.size = size
+
+    @property
+    def size(self):
+        """ These are a getter and setter when to get
+        the current size of the square"""
+        return self.__size
+
+    @size.setter
+    def __init__(self, value):
+        """
+        Initializes instance of a square
+        Arguments:
+            size
+        """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if (ValueError < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns area of square based on size
+        """
+        return (self.__size*self.__size)
