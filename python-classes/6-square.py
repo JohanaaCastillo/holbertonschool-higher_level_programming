3e681ce4ccdd92efef0848c7066bb86f5ff97cb5
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-15 19:41:01 -0500

    Adding size, setter and property

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,9 @@ class Square():
     """
     Creates a square class object
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
+        self.position = position
 
     @property
     def size(self,):
@@ -26,6 +27,21 @@ class Square():
         if (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """These are getter and setter whose to get/set
+        the current position of the new square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """
