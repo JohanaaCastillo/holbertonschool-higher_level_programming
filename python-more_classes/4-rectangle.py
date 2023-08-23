7d5d193be97817e9dde12c2f895f94a87cbf1574
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-23 09:42:55 -0500

    Update

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -69,10 +69,10 @@ class Rectangle:
         """
         if not self.perimeter:
             return ""
-        return('\n'.join('#' * self.width for x in range(self.height)))
+        return ('\n'.join('#' * self.width for x in range(self.height)))
 
     def __repr__(self):
         """modifies repr object
 
         """
-        return("Rectangle({}, {})".format(self.width, self.height))
+        return ("Rectangle({}, {})".format(self.width, self.height))
