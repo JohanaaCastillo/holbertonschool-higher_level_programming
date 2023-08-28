518f741ecd442097bd3c89159916a9df80d40955
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-28 10:12:07 -0500

    script that lists all records of the table second_table of the database hbtn_0c_0 and should be ordered by score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- A script that lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table
+-- ORDER BY : Ordena los registros resultantes de una consulta por un campo o campos 
+-- especificados en ordene ascendente(ASC) o descendente(DESC)
+ORDER BY score DESC;
