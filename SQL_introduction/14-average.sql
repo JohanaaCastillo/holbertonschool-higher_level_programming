719a395ab368bcae30f696fe3c50095570e957f4
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-28 11:23:18 -0500

    add as score

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- A script that computes the score average of all records in the table second_table of the database hbtn_0c_0
 -- AVG() function is an aggregate function that allows you to calculate the average value of a set.
-SELECT AVG(score)
+SELECT AVG(score) as score
 FROM second_table;
